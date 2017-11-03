88de52790a12a545ffbca3565484c9d1354e39de
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-11-03 12:13:04 +0100

    Bug Fixing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SRW'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 ISRELEASED = False
 
 DESCRIPTION = 'SRW in OASYS'
