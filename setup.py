e35a970078db2bbbdf0a400006a3575ff1e2f551
Luca Rebuffi <luca.rebuffi@elettra.eu>
2018-06-05 18:43:37 +0200

    Version 1.0.9: First release of SRW in OASYS (with wofrysrw 1.0.10)
    Pre-compiled libraries for Ubuntu, OSx, Windows 10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SRW'
-VERSION = '1.0.8'
+VERSION = '1.0.9'
 ISRELEASED = False
 
 DESCRIPTION = 'SRW in OASYS'
