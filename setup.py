4d92bd04a2b89306cecedc55a4a946bfa7ecfefb
lucarebuffi <luca.rebuffi@elettra.eu>
2019-01-23 11:12:27 -0600

    New DABAM interface

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SRW'
-VERSION = '1.0.56'
+VERSION = '1.0.57'
 ISRELEASED = False
 
 DESCRIPTION = 'SRW in OASYS'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.1.26',
+    'oasys1>=1.1.30',
     'oasys1-srwlib>=1.0.16',
     'wofrysrw>=1.0.26'
 )
