cdf2313c250a64f45beb484d1ce0e06ce5538249
lucarebuffi <luca.rebuffi@elettra.eu>
2019-01-23 16:39:39 -0600

    Wofry to SRW: added curvature parameters

--- orangecontrib/srw/widgets/tools/ow_from_wofry_wavefront.py
+++ orangecontrib/srw/widgets/tools/ow_from_wofry_wavefront.py
@@ -1,9 +1,8 @@
 from orangewidget import gui
+from orangewidget.settings import Setting
 from oasys.widgets import gui as oasysgui
-
 from oasys.widgets import widget
 
-from PyQt5.QtGui import QPalette, QColor, QFont
 from PyQt5.QtWidgets import QApplication, QMessageBox
 from PyQt5.QtCore import QRect
 
@@ -29,9 +28,15 @@ class OWFromWofryWavefront2d(widget.OWWidget):
                 "doc":"SRWData",
                 "id":"SRWData"}]
 
-    CONTROL_AREA_WIDTH = 605
+    CONTROL_AREA_WIDTH = 405
 
     want_main_area = 0
+
+    Rx = Setting(10.0)
+    dRx = Setting(0.001)
+
+    Ry = Setting(10.0)
+    dRy = Setting(0.001)
 
     wavefront = None
 
@@ -45,23 +50,21 @@ class OWFromWofryWavefront2d(widget.OWWidget):
                                round(min(geom.width()*0.98, self.CONTROL_AREA_WIDTH+10)),
                                round(min(geom.height()*0.95, 100))))
 
-        self.setMaximumHeight(self.geometry().height())
+        self.setFixedHeight(200)
         self.setMaximumWidth(self.geometry().width())
 
         self.controlArea.setFixedWidth(self.CONTROL_AREA_WIDTH)
 
+        box = oasysgui.widgetBox(self.controlArea, "SRW Wavefront Setting", addSpace=False, orientation="horizontal", height=100)
 
-        label = gui.label(self.controlArea, self, "From Wofry Wavefront To SRW Wavefront")
-        font = QFont(label.font())
-        font.setBold(True)
-        font.setItalic(True)
-        font.setPixelSize(14)
-        label.setFont(font)
-        palette = QPalette(label.palette()) # make a copy of the palette
-        palette.setColor(QPalette.Foreground, QColor('Dark Blue'))
-        label.setPalette(palette) # assign new palette
+        box_1 = oasysgui.widgetBox(box, "", addSpace=False, orientation="vertical", height=60)
+        box_2 = oasysgui.widgetBox(box, "", addSpace=False, orientation="vertical", height=60)
 
-        gui.separator(self.controlArea, 10)
+        oasysgui.lineEdit(box_1, self, "Rx", "Rx",labelWidth=50, valueType=float, orientation="horizontal")
+        oasysgui.lineEdit(box_1, self, "dRx", "dRx",labelWidth=50, valueType=float, orientation="horizontal")
+
+        oasysgui.lineEdit(box_2, self, "Ry", "Ry",labelWidth=50, valueType=float, orientation="horizontal")
+        oasysgui.lineEdit(box_2, self, "dRy", "dRy",labelWidth=50, valueType=float, orientation="horizontal")
 
         gui.button(self.controlArea, self, "Convert", callback=self.convert_wavefront, height=45)
 
@@ -76,7 +79,13 @@ class OWFromWofryWavefront2d(widget.OWWidget):
     def convert_wavefront(self):
         if not self.wavefront is None:
             try:
-                self.send("SRWData", SRWData(srw_wavefront=SRWWavefront.fromGenericWavefront(self.wavefront)))
+                srw_wavefront     = SRWWavefront.fromGenericWavefront(self.wavefront)
+                srw_wavefront.Rx  = self.Rx
+                srw_wavefront.dRx = self.dRx
+                srw_wavefront.Ry  = self.Ry
+                srw_wavefront.dRy = self.dRy
+
+                self.send("SRWData", SRWData(srw_wavefront=srw_wavefront))
             except Exception as exception:
                 QMessageBox.critical(self, "Error", str(exception), QMessageBox.Ok)
 
