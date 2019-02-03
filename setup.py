6149c1f3c93856c2c5e9c54f1e102d94bb95e746
lucarebuffi <luca.rebuffi@elettra.eu>
2019-02-03 12:36:07 -0600

    Bug Fixing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SRW'
-VERSION = '1.0.69'
+VERSION = '1.0.70'
 ISRELEASED = False
 
 DESCRIPTION = 'SRW in OASYS'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.1.35',
+    'oasys1>=1.1.36',
     'oasys1-srwlib>=1.0.21',
     'wofrysrw>=1.0.26'
 )
