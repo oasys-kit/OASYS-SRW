1d5bbfacf880ad8dfc7021e7571153bb4b3e8636
lucarebuffi <lrebuffi@anl.gov>
2019-06-18 18:45:36 -0500

    New CRL+Hybrid+Thickness Error

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SRW'
-VERSION = '1.1.5'
+VERSION = '1.1.6'
 ISRELEASED = False
 
 DESCRIPTION = 'SRW in OASYS'
