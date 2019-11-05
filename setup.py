209091a7ad1a3422aafdd7a45f951d6f96e125fe
lucarebuffi <lrebuffi@anl.gov>
2019-11-05 17:09:40 -0600

    New Features

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SRW'
-VERSION = '1.1.26'
+VERSION = '1.1.27'
 ISRELEASED = False
 
 DESCRIPTION = 'SRW in OASYS'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.2.16',
+    'oasys1>=1.2.21',
     'wofrysrw>=1.1.3'
 )
 
