cdec6efcd1a5cc88e0f98690db4531401da563c2
lucarebuffi <lrebuffi@anl.gov>
2020-05-05 20:40:35 -0500

    Revert "PyQt 5.14 bug fixed"
    This reverts commit 9ebc82b66c41b5e380c2bb90699e7c731f4fec5b.
    
    # Conflicts:
    #	setup.py

--- orangecontrib/srw/widgets/optical_elements/ow_srw_filter.py
+++ orangecontrib/srw/widgets/optical_elements/ow_srw_filter.py
@@ -28,38 +28,19 @@ class OWSRWFilter(OWSRWOpticalElement):
               ("Thickness Error Data", OasysThicknessErrorsData, "setThicknessErrorProfile"),
               WidgetDecorator.syned_input_data()[0]]
 
-    horizontal_shift = Setting(0.0)
-    vertical_shift = Setting(0.0)
-
-    width = Setting(0.001)
-    height = Setting(0.001)
-
-    horizontal_number_of_points = Setting(100)
-    vertical_number_of_points = Setting(100)
-
     material = Setting("Be")
     thickness = Setting(500)
     has_thickness_error = Setting(0)
     thickness_error_profile = Setting("")
+    scaling_factor = Setting(1.0)
 
     def __init__(self):
         super().__init__(has_orientation_angles=False)
 
     def draw_specific_box(self):
-        shape_box = oasysgui.widgetBox(self.tab_bas, "Shape", addSpace=True, orientation="vertical")
-
-        oasysgui.lineEdit(shape_box, self, "horizontal_shift", "Horizontal Shift [m]", labelWidth=260, valueType=float, orientation="horizontal")
-        oasysgui.lineEdit(shape_box, self, "vertical_shift", "Vertical Shift [m]", labelWidth=260, valueType=float, orientation="horizontal")
-
-        oasysgui.lineEdit(shape_box, self, "width", "Width [m]", labelWidth=260, valueType=float, orientation="horizontal")
-        oasysgui.lineEdit(shape_box, self, "height", "Height [m]", labelWidth=260, valueType=float, orientation="horizontal")
-
-        oasysgui.lineEdit(shape_box, self, "horizontal_number_of_points", "Horizontal Nr. Points", labelWidth=260, valueType=int, orientation="horizontal")
-        oasysgui.lineEdit(shape_box, self, "vertical_number_of_points", "Vertical Nr, Points", labelWidth=260, valueType=int, orientation="horizontal")
-
         material_box = oasysgui.widgetBox(self.tab_bas, "Material", addSpace=True, orientation="vertical")
 
-        oasysgui.lineEdit(material_box, self, "material", "Chemical Formula", labelWidth=260, valueType=str, orientation="horizontal")
+        oasysgui.lineEdit(material_box, self, "material", "Material", labelWidth=260, valueType=str, orientation="horizontal")
         oasysgui.lineEdit(material_box, self, "thickness", "Thickness [\u03bcm]", labelWidth=260, valueType=float, orientation="horizontal")
 
         gui.comboBox(material_box, self, "has_thickness_error", label="Use Thickness Error Profile",
@@ -68,13 +49,15 @@ class OWSRWFilter(OWSRWOpticalElement):
 
         gui.separator(material_box)
 
-        self.error_box_1 = oasysgui.widgetBox(material_box, "", addSpace=False, orientation="vertical", height=30)
-        self.error_box_2 = oasysgui.widgetBox(material_box, "", addSpace=False, orientation="vertical", height=30)
+        self.error_box_1 = oasysgui.widgetBox(material_box, "", addSpace=False, orientation="vertical", height=60)
+        self.error_box_2 = oasysgui.widgetBox(material_box, "", addSpace=False, orientation="vertical", height=60)
 
         file_box =  oasysgui.widgetBox(self.error_box_2, "", addSpace=False, orientation="horizontal")
 
-        self.le_thickness_error_profile = oasysgui.lineEdit(file_box, self, "thickness_error_profile", "Thickness error data file", labelWidth=155, valueType=str, orientation="horizontal")
+        self.le_thickness_error_profile = oasysgui.lineEdit(file_box, self, "thickness_error_profile", "Error data file", labelWidth=100, valueType=str, orientation="horizontal")
         gui.button(file_box, self, "...", callback=self.selectThicknessErrorProfile)
+
+        oasysgui.lineEdit(self.error_box_2, self, "scaling_factor", "Error Scaling Factor", labelWidth=260, valueType=float, orientation="horizontal")
 
         self.set_ThicknessError()
 
@@ -86,32 +69,29 @@ class OWSRWFilter(OWSRWOpticalElement):
         self.le_thickness_error_profile.setText(oasysgui.selectFileFromDialog(self, self.thickness_error_profile, "Thickness error data file", file_extension_filter="*.h5"))
 
     def get_optical_element(self):
+        energy = self.input_srw_data.get_srw_wavefront().get_photon_energy()
+        mesh = self.input_srw_data.get_srw_wavefront().mesh
+
         return SRWFilter(name=self.name,
                          material=self.material,
                          thickness=self.thickness*1e-6,
-                         x_range=[self.horizontal_shift - self.width/2, self.horizontal_shift + self.width/2],
-                         y_range=[self.vertical_shift - self.height/2, self.vertical_shift + self.height/2],
-                         n_points_x=self.horizontal_number_of_points,
-                         n_points_y=self.vertical_number_of_points,
-                         energy=self.input_srw_data.get_srw_wavefront().get_photon_energy(),
-                         thickness_error_profile=None if self.has_thickness_error==0 else self.thickness_error_profile)
+                         x_range=[mesh.xStart, mesh.xFin],
+                         y_range=[mesh.yStart, mesh.yFin],
+                         n_points_x=mesh.nx,
+                         n_points_y=mesh.ny,
+                         energy=energy,
+                         thickness_error_profile=None if self.has_thickness_error==0 else self.thickness_error_profile,
+                         scaling_factor=self.scaling_factor)
 
     def check_data(self):
         super().check_data()
 
-        congruence.checkNumber(self.horizontal_shift, "Horizontal Shift")
-        congruence.checkNumber(self.vertical_shift, "Vertical Shift")
-
-        congruence.checkStrictlyPositiveNumber(self.width, "Width")
-        congruence.checkStrictlyPositiveNumber(self.height, "Height")
-
-        congruence.checkStrictlyPositiveNumber(self.horizontal_number_of_points, "Horizontal Nr. Points")
-        congruence.checkStrictlyPositiveNumber(self.vertical_number_of_points, "Vertical Nr. Points")
-
-        congruence.checkEmptyString(self.material, "Chemical Formula")
+        congruence.checkEmptyString(self.material, "Material")
         congruence.checkStrictlyPositiveNumber(self.thickness, "Thickness")
 
-        if self.has_thickness_error == 1: congruence.checkFile(self.thickness_error_profile)
+        if self.has_thickness_error == 1:
+            congruence.checkFile(self.thickness_error_profile)
+            congruence.checkStrictlyPositiveNumber(self.scaling_factor, "Scaling Factor")
 
     def receive_specific_syned_data(self, optical_element):
         if not optical_element is None:
