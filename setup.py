549fb7c6eb737651a4864155940b0f83a7fd57ae
lucarebuffi <lrebuffi@anl.gov>
2020-05-06 13:53:51 -0500

    Revert "PyQt 5.14 bug fixed"
    This reverts commit 9ebc82b66c41b5e380c2bb90699e7c731f4fec5b.
    
    # Conflicts:
    #	setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SRW'
-VERSION = '1.1.43'
+VERSION = '1.1.44'
 ISRELEASED = False
 
 DESCRIPTION = 'SRW in OASYS'
@@ -41,7 +41,7 @@ SETUP_REQUIRES = (
 
 INSTALL_REQUIRES = (
     'oasys1>=1.2.32',
-    'wofrysrw>=1.1.5',
+    'wofrysrw>=1.1.6',
     'scikit-image'
 )
 
