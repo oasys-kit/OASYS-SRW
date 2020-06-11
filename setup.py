89daf1bcbc40e394588692c66c76a758f967557f
lucarebuffi <lrebuffi@anl.gov>
2020-06-10 21:07:06 -0500

    New Developments

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SRW'
-VERSION = '1.1.46'
+VERSION = '1.1.48'
 ISRELEASED = False
 
 DESCRIPTION = 'SRW in OASYS'
