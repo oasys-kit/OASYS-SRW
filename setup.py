3bb955493c0e6f1f5a19496e59c93660f3adc731
lucarebuffi <lrebuffi@anl.gov>
2020-06-29 12:13:15 -0500

    New Developments

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SRW'
-VERSION = '1.1.54'
+VERSION = '1.1.55'
 ISRELEASED = False
 
 DESCRIPTION = 'SRW in OASYS'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.2.39',
+    'oasys1>=1.2.40',
     'wofrysrw>=1.1.8',
     'scikit-image'
 )
